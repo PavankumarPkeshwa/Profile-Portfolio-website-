3f2675e3cc6e6495bf151e6f77ecc7a1b4df1d4f
Pavan Kumar P <98679889+PavankumarPkeshwa@users.noreply.github.com>
2025-03-28 10:55:05 +0530

    Update profile.js

--- profile.js
+++ profile.js
@@ -47,3 +47,15 @@ ScrollReveal().reveal('.home-content,.heading',{origin:'top'});
 ScrollReveal().reveal('.home-img,.services-container,.portfolio-box,.contact form',{origin:'bottom'});
 ScrollReveal().reveal('.home-content h1, .about-img',{origin:'left'});
 ScrollReveal().reveal('.home-content p, .about-content',{origin:'right'});
+
+
+
+//Multiple text  display
+
+const typed =new Typed('.multiple-text',{
+strings:['Java Full Stack Developer','Front-End Developer','Backend-Developer','Blogger'],
+typeSpeed:100,
+backSpeed:100,
+backDelay:1000,
+loop:true
+});
