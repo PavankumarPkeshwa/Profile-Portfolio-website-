2fa9469850692e5d3a665ed2d8e3eefd19db4bd9
Pavan Kumar P <98679889+PavankumarPkeshwa@users.noreply.github.com>
2025-04-01 11:25:22 +0530

    Update profile.js

--- profile.js
+++ profile.js
@@ -44,8 +44,9 @@ ScrollReveal({
     delay:200
 });
 
-ScrollReveal().reveal('.home-img,.services-container,.portfolio-box,.contact form',{origin:'top'});
-ScrollReveal().reveal('.home-content,.heading',{origin:'bottom'});
+
+ScrollReveal().reveal('.home-content,.heading',{origin:'top'});
+ScrollReveal().reveal('.home-img,.services-container,.portfolio-box,.contact form',{origin:'bottom'});
 ScrollReveal().reveal('.home-content h1, .about-img',{origin:'left'});
 ScrollReveal().reveal('.home-content p, .about-content,.list',{origin:'right'});
 
