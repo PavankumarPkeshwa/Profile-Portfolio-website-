dc5ea0c96e185ca86bf374e065f118e328cbceb6
Pavan Kumar P <98679889+PavankumarPkeshwa@users.noreply.github.com>
2026-01-16 08:49:55 +0000

    updated config file

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,15 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,          // 🔥 REQUIRED for Codespaces
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:5000', // ✅ safer than localhost
-        changeOrigin: true,
-        secure: false
+        target: 'http://localhost:5000',
+        changeOrigin: true
       }
     }
   }
 })
-
